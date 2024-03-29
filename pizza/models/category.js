a1d9258995fcfc523786f995577434053dfe66ee
Mustafa Karagöz <karagozhalitmustafa@gmail.com>
2024-03-30 00:25:27 +0300

    Modüller oluşturuldu.

---
+++ pizza/models/category.js
@@ -0,0 +1,9 @@
+class Category {
+    constructor(id, title, color){
+        this.id = id;    // class'a gelen değerleri bind ettim.(bir fonksiyonun parametrelerini belirili bir değere bağlamak):
+        this.title = title;
+        this.color = color;
+    }
+}
+
+export default Category;
---
+++ pizza/models/pizza.js
@@ -0,0 +1,23 @@
+class Pizza {
+    constructor(
+        id,
+        categoryIds,
+        title,
+        affordability,
+        complexity,
+        imageUrl,
+        ingerdients, //içindekiler
+
+    ) {
+        this.id= id;
+        this.categoryIds = categoryIds,
+        this.title = title,
+        this.affordability = affordability,
+        this.complexity = complexity,
+        this.imageUrl = imageUrl,
+        this.ingerdients
+        
+    }
+}
+
+export default Pizza;
