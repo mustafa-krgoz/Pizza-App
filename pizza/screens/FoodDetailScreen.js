3b56e69784ae8c61be6d7fe521b60ed062fe938f
Mustafa Karagöz <karagozhalitmustafa@gmail.com>
2024-05-13 16:20:17 +0300

    Yemek detay sayfasının tasarımı

--- pizza/screens/FoodDetailScreen.js
+++ pizza/screens/FoodDetailScreen.js
@@ -1,46 +1,72 @@
-import { StyleSheet, Text, View, ScrollView, Image} from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import React from 'react';
+
 import { Pizzalar } from '../data/dummy-data';
 import FoodIngredients from '../components/FoodIngredients';
 
-export default function FoodDetailScreen({route}) {
+export default function FoodDetailScreen({ route }) {
   const foodId = route.params.foodId;
-  const selectedFood = Pizzalar.find((food)=>food.id === foodId);
+  const selectedFood = Pizzalar.find((food) => food.id === foodId);
   console.log(selectedFood);
+
   return (
-    <ScrollView>
-        <Image style ={styles.image} source={{uri:selectedFood.imageUrl}}/>
-        <View style={styles.details}>
-            <Text style={styles.detailItem}>{selectedFood.complexity}</Text>
-            <Text style={styles.detailItem}>{selectedFood.affordability}</Text>
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedFood.imageUrl }} />
+      <Text style={styles.title}>{selectedFood.title}</Text>
+      <View style={styles.details}>
+        <Text style={styles.detailItem}>{selectedFood.complexity}</Text>
+        <Text style={styles.detailItem}>{selectedFood.affordability}</Text>
+      </View>
+      <View style={styles.listContainer}>
+        <View style={styles.subTitleContainer}>
+          <Text style={styles.subTitle}>İçindekiler</Text>
         </View>
-        <View>
-            <View>
-                <Text>
-                    <Text>İçindekiler</Text>
-                </Text>
-            </View>
-            <FoodIngredients data={selectedFood.ingerdients}/>
-        </View>
+        <FoodIngredients data={selectedFood.ingerdients} />
+      </View>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    image: {
-        width: '%100',
-        height: 300,
-    },
+  rootContainer: {
+    marginBottom: 50,
+  },
 
-    details: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 5,
-      },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
 
-      detailItem: {
-        marginHorizontal: 4,
-        fontSize: 12,
-      },
-})
+  details: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 5,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+    color: 'red',
+  },
+  listContainer: {
+    width: '100%',
+    paddingHorizontal: 10,
+  },
+  subTitleContainer: {
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: 'orange',
+    marginVertical: 5,
+  },
+  subTitle: {
+    color: 'orange',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
